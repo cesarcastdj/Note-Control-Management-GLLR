4ced820d10b8f87ddb5cdc869d8c720607292d44
Cesar <cesaucast@gmail.com>
2025-05-25 02:02:12 -0400

    UPDATE: Se regreso al Commit anterio Donde el server funcionaba, Esta en configuracion, con el modulo de estudiantes, Que esta en desarrollo.

--- src/layouts/Students/StudentDashboard.tsx
+++
@@ -1,9 +0,0 @@
-// Crear un archivo base para el apartado de estudiantes
-export default function StudentDashboard() {
-  return (
-    <div>
-      <h1>Panel de Estudiantes</h1>
-      <p>Bienvenido al panel de estudiantes.</p>
-    </div>
-  );
-}
